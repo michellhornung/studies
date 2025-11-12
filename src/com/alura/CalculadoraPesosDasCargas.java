8ae25ce173df34eaf5c17bedd4ee1e60bb4d08b3
Michell Hornung <michellhornung@gmail.com>
2025-11-12 15:53:48 -0300

    Calculadora pesoa das cargas

---
+++ src/com/alura/CalculadoraPesosDasCargas.java
@@ -0,0 +1,18 @@
+package com.alura;
+
+public class CalculadoraPesosDasCargas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double pesoCarga1 = 1033.49;
+	    double pesoCarga2 = 849.88;
+
+	    int pesoTotalCargas = (int) (pesoCarga1 + pesoCarga2);
+
+	    // O resultado deve ser 1883
+	    System.out.println("Peso total das cargas: " + pesoTotalCargas);
+	  }
+
+	}
+
